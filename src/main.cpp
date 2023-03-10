0ab66fd683b46a3730219c753448d46f4fec68cd
Caden Shmookler <cshmookler@gmail.com>
2023-03-10 12:49:51 -0700

    Removed unnecessary example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
 
-namespace bar {
-	void print() { std::cout << "This is NOT what I want" << std::endl; }
-}
-
-namespace foo {
-	namespace bar {
-		void print() { std::cout << "This is what I want" << std::endl; }
-	}
-
-	void test() { ::bar::print(); }
-}
-
 int main(int argc, char** argv)
 {
-	foo::test();
+	std::cout << "Hello World!" << std::endl;
 
 	return 0;
 }
